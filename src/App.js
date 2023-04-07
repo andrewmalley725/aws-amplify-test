412a7798a61d84d1a10e46fe5881a80f0bbefe89
andrewmalley725 <andrew.alley99@gmail.com>
2023-04-07 09:18:48 -0600

    test commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import GameContainer from "./components/gameContainer";
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <GameContainer/>,
+  },
+  {
+      path: "/test",
+      element: <h1>it worked</h1>,
+  }
+]);
 
 function App() {
   return (
-    <GameContainer/>
+    <RouterProvider router={router} />
   );
 }
 
